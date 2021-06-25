3e0869c419cda29a81a7b5e3dd25bf848b9fbe54
Aravinda Guruprasad <aguruprasad@uei.com>
2021-06-25 12:12:58 +0530

    Changed img to Image

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 function Avatar({ url, className }) {
   return (
-    <img
+    <Image
       className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
       src='/images/avatar.jpg'
       alt='ProfilePicture'
-      loading='lazy'
+      height={40}
+      width={40}
     />
   );
 }
